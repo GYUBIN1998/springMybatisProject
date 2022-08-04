1a9e2be08538c351702ee1f00df7b94de483015a
Kevin <kvpark98@naver.com>
2022-08-04 10:05:55 +0900

    프로젝트 시작!

---
+++ springProject3/src/test/java/com/acorn/springProject3/SpringProject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acorn.springProject3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProject3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
